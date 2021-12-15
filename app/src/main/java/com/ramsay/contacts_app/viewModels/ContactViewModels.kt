78ce472b69b0ba3af0fb952b8fec8159c38b095f
Ramsay2 <sachin114sharma@gmail.com>
2021-12-16 02:33:37 +0530

    commit

--- app/src/main/java/com/ramsay/contacts_app/viewModels/ContactViewModels.kt
+++ app/src/main/java/com/ramsay/contacts_app/viewModels/ContactViewModels.kt
@@ -1,27 +1,29 @@
 package com.ramsay.contacts_app.viewModels
 
 import android.content.Context
-import android.provider.ContactsContract
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.ViewModel
+import androidx.paging.PagingData
 import com.ramsay.contacts_app.models.Contacts
 import com.ramsay.contacts_app.repository.ContactRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
 @HiltViewModel
-class ContactViewModels @Inject constructor(private val contactRepository: ContactRepository):ViewModel() {
+class ContactViewModels
+@Inject constructor(private val contactRepository: ContactRepository):ViewModel() {
 
 
-    fun pagingData(contacts:ArrayList<ContactsContract.Contacts>)
-    = contactRepository.getPagingData()
+    fun pagingData(): LiveData<PagingData<Contacts>> = contactRepository.getPagingData()
 
     suspend fun saveToDb(context: Context){
         contactRepository.saveToDB(context)
 
     }
 
-    fun getContactFromDB(contacts: Contacts):LiveData<List<Contacts>>{
+    fun getContactFromDB():LiveData<List<Contacts>>{
+        Log.d("TAG987", "getContactFromDB: ${contactRepository.getContacts().value}")
         return contactRepository.getContacts()
     }
 
