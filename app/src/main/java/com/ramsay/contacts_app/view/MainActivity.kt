78fdadbd1beb18c7029ba3a9f6f0f911188b60b6
Ramsay2 <sachin114sharma@gmail.com>
2021-12-24 02:39:48 +0530

    commit

--- app/src/main/java/com/ramsay/contacts_app/view/MainActivity.kt
+++ app/src/main/java/com/ramsay/contacts_app/view/MainActivity.kt
@@ -1,34 +1,63 @@
 package com.ramsay.contacts_app.view
 
-import androidx.appcompat.app.AppCompatActivity
-import android.os.Bundle
 import android.Manifest
 import android.os.Build
-import android.webkit.PermissionRequest
+import android.os.Bundle
+import android.util.Log
+import androidx.activity.viewModels
 import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.karumi.dexter.Dexter
 import com.karumi.dexter.MultiplePermissionsReport
 import com.karumi.dexter.PermissionToken
 import com.karumi.dexter.listener.multi.MultiplePermissionsListener
-import com.ramsay.contacts_app.R
+import com.ramsay.contacts_app.databinding.ActivityMainBinding
+import com.ramsay.contacts_app.models.Contacts
+import com.ramsay.contacts_app.viewModels.ContactViewModels
+import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), OnContactClick {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var contactAdapter: ContactAdapter
+    private var contactList = mutableListOf<Contacts>()
+    private val contactViewModel: ContactViewModels by viewModels()
 
 
-class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         runTimePermission()
+
+
     }
 
 
 
     private fun runTimePermission() {
         Dexter.withContext(this).withPermissions(
-            Manifest.permission.READ_CONTACTS,
             Manifest.permission.READ_CONTACTS
         ).withListener(object : MultiplePermissionsListener {
             @RequiresApi(Build.VERSION_CODES.N)
             override fun onPermissionsChecked(multiplePermissionsReport: MultiplePermissionsReport) {
+
+
+                CoroutineScope(Dispatchers.Main).launch {
+                    contactViewModel.saveToDb(applicationContext)
+
+                }
+                setRecyclerview()
+                getContact()
 
             }
 
@@ -40,4 +69,43 @@ class MainActivity : AppCompatActivity() {
             }
         }).check()
     }
+
+    private fun setRecyclerview() {
+        contactAdapter = ContactAdapter(contactList, this)
+        binding.recyclerview.apply {
+            layoutManager = LinearLayoutManager(this@MainActivity)
+            adapter = contactAdapter
+        }
+    }
+
+    private fun getContact() {
+
+       contactViewModel.getContactFromDB().observe(this, { it ->
+            contactList.clear()
+            contactList.addAll(it)
+          //  contactList.sortBy { it.contactCount }
+            contactAdapter.notifyDataSetChanged()
+            Log.d("TAG1234", "getContact: $contactList")
+
+        })
+
+
+    }
+
+    override fun onClick(contacts: Contacts) {
+
+         val count = contacts.contactCount
+
+         if (count != null) {
+             contacts.contactCount = count + 1
+         }
+        CoroutineScope(Dispatchers.IO).launch {
+            contactViewModel.updateContact(contacts)
+        }
+
+
+
+
+    }
+
 }
